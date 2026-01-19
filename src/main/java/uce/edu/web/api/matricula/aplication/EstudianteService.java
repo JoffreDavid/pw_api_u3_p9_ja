6a39648eac29479c79056b3f999a483c33aea3e2
JoffreDavid <jdariasb@uce.edu.ec>
2026-01-18 19:41:49 -0500

    Taller 26 Verbos http en api Quarkus

--- src/main/java/uce/edu/web/api/matricula/aplication/EstudianteService.java
+++ src/main/java/uce/edu/web/api/matricula/aplication/EstudianteService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import uce.edu.web.api.matricula.domain.Estudiante;
 import uce.edu.web.api.matricula.infraestructure.EstudianteRepository;
 
@@ -13,7 +14,47 @@ public class EstudianteService {
     @Inject
     private EstudianteRepository estudianteRepository;
 
+   
     public List<Estudiante> listarTodos(){
         return this.estudianteRepository.listAll();
+    }
+
+  
+    public Estudiante consultarPorId(Integer id){
+        return this.estudianteRepository.findById(id.longValue());
+    }
+
+    @Transactional
+    public void crear(Estudiante estu){
+        this.estudianteRepository.persist(estu);
+    }
+
+    @Transactional
+    public void actualizar(Integer id, Estudiante est){
+        Estudiante estu = this.consultarPorId(id);
+
+        estu.setApellido(est.getApellido());
+        estu.setNombre(est.getNombre());
+        estu.setFechaNacimiento(est.getFechaNacimiento());  
+        //se actualiza directamente por dirtychecking
+    }
+
+    @Transactional
+    public void actualizarParcial(Integer id, Estudiante est){
+        Estudiante estu = this.consultarPorId(id);
+        if(est.getApellido() != null){
+            estu.setApellido(est.getApellido());
+        }
+        if(est.getNombre() != null){
+            estu.setNombre(est.getNombre());
+        }
+        if(est.getFechaNacimiento() != null){
+            estu.setFechaNacimiento(est.getFechaNacimiento());
+        }
+    }
+
+    @Transactional
+    public void eliminar(Integer id){
+        this.estudianteRepository.deleteById(id.longValue());
     }
 }
